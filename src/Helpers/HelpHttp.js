65d6543a9344b7c3553c193e977ae9a96a752aa8
marco cardenas <mardcart.9@gmail.com>
2023-08-30 18:53:16 -0300

    se crea el archivo helpers para las peticiones

---
+++ src/Helpers/HelpHttp.js
@@ -0,0 +1,21 @@
+export const HelpHttp =()=>{
+
+    const CustomFetch =(endpoint,options)=>{
+        //PONER LO QUE ACEPTA EN LAS CABECERA
+        const defaultHeader ={
+            accept:"application/json",
+        };
+    }
+
+    const get =()=>{}
+    const post =()=>{}
+    const put =()=>{}
+    const del =()=>{}
+
+    return {
+        get,
+        post,
+        put,
+        del,
+    };
+}
